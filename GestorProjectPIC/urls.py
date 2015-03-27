0b915fe314d61d462f893df48f0aa26e773a189d
paofigue <paofigue@gmail.com>
2015-03-27 11:28:20 -0400

    recuperar contrasenha-mejorado2

--- GestorProjectPIC/urls.py
+++ GestorProjectPIC/urls.py
@@ -12,5 +12,6 @@ urlpatterns = patterns('',
     url(r'^reset/password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
     url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
+    url(r'^accounts/login/', include(admin.site.urls) , name='login'),
 
 )
