ad35fcaf734568173984761a34bd92d445748157
paofigue <paofigue@gmail.com>
2015-03-30 11:15:54 -0400

    actualizar vista Proyeto

--- PIC/models.py
+++ PIC/models.py
@@ -7,9 +7,9 @@ from django.contrib.auth.models import User
 class Proyecto(models.Model):
     nombre = models.CharField(max_length=60)
     descripcion = models.CharField(max_length=120)
-    fechaCreacion = models.DateTimeField()
-    fechaInicio = models.DateTimeField('date published')
-    fechaFin = models.DateTimeField()
+    fechaCreacion = models.DateTimeField('Fecha de Creacion')
+    fechaInicio = models.DateTimeField('Fecha de Inicio')
+    fechaFin = models.DateTimeField('Fecha de Fin')
     duracionEstimada = models.CharField(max_length=20)
     estado = models.CharField(max_length=40)
     usuarios = models.ManyToManyField(User, related_name='proyectos')
